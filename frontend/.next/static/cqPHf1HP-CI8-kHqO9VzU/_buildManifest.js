af5fa79218367000c8695a9c71b8e4eb42c23b2c
rp948 <rp948@njit.edu>
2024-03-24 18:41:21 -0400

    add .next

---
+++ frontend/.next/static/cqPHf1HP-CI8-kHqO9VzU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/c16184b3-41c5fef8871a3b4a.js","static/css/a5362a57321df25c.css","static/chunks/pages/index-f6cf4928dd02e7ad.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
