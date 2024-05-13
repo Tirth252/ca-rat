119f21d8531d1b521acd99fb3bfdee827e95d16e
Tirth Panchasara <panchasara.tirth99@gmail.com>
2024-05-13 14:23:36 -0400

    Created urls.py in main and registered Jewelary model in admin

--- backend/caart/caart/settings.py
+++ backend/caart/caart/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-import environ
+import environ # type: ignore
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
